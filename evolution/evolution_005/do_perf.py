894173be3ff4774f092de0a175abef65665fb2da
lanzhiwang <hzhilamp@163.com>
2019-10-31 18:38:34 +0800

    complent add performance script

--- evolution/evolution_005/do_perf.py
+++ evolution/evolution_005/do_perf.py
@@ -33,6 +33,31 @@ parser.feed_disconnect()
     for dataset in ['http10long', 'websites']:
         for parser in ['cffi', 'cext']:
             print('-- {} {} --'.format(dataset, parser))
+            print([
+                'python', '-m', 'perf', 'timeit', '-s', setup.format(parser, dataset), loop])
             subprocess.check_call([
                 'python', '-m', 'perf', 'timeit', '-s', setup.format(parser, dataset), loop])
             print()
+
+"""
+-- http10long cffi --
+['python', '-m', 'perf', 'timeit', '-s', '\nimport parsers\nimport do_perf\nparser, *_ = parsers.make_cffi(lambda: parsers.silent_callback)\ndata = do_perf.get_http10long()\n', '\nparser.feed(data)\nparser.feed_disconnect()\n']
+
+-- http10long cext --
+['python', '-m', 'perf', 'timeit', '-s', '\nimport parsers\nimport do_perf\nparser, *_ = parsers.make_cext(lambda: parsers.silent_callback)\ndata = do_perf.get_http10long()\n', '\nparser.feed(data)\nparser.feed_disconnect()\n']
+
+-- websites cffi --
+['python', '-m', 'perf', 'timeit', '-s', '\nimport parsers\nimport do_perf\nparser, *_ = parsers.make_cffi(lambda: parsers.silent_callback)\ndata = do_perf.get_websites()\n', '\nparser.feed(data)\nparser.feed_disconnect()\n']
+
+-- websites cext --
+['python', '-m', 'perf', 'timeit', '-s', '\nimport parsers\nimport do_perf\nparser, *_ = parsers.make_cext(lambda: parsers.silent_callback)\ndata = do_perf.get_websites()\n', '\nparser.feed(data)\nparser.feed_disconnect()\n']
+
+python -m perf timeit -s
+import parsers
+import do_perf
+parser, *_ = parsers.make_cext(lambda: parsers.silent_callback)
+data = do_perf.get_websites()
+parser.feed(data)
+parser.feed_disconnect()
+
+"""
