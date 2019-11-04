5c2d1e740efac702b256d0b4eadac55c29b29e68
lanzhiwang <hzhilamp@163.com>
2019-11-04 13:54:19 +0800

    app

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 * https://github.com/squeaky-pl/japronto/commits/master?before=a526277a2f59100388c9f39d4ca22bfb4909955b+630
 
-* git checkout -b evolution_16 51c07158905dc22488b9f0a75972537e55760927
+* git checkout -b evolution_17 51c07158905dc22488b9f0a75972537e55760927
 
 * https://github.com/wolever/python-cffi-example
 
---
+++ evolution/evolution_015/server.py
@@ -0,0 +1,40 @@
+import asyncio
+import uvloop
+import signal
+import argparse
+import sys
+
+import protocols.handler
+
+
+from router.cmatcher import Matcher
+from router import Router
+from app import Application
+
+
+def slash(request, transport, response):
+    response.__init__(text='Hello slash!')
+
+    transport.write(response.render())
+
+
+def hello(request, transport, response):
+    response.__init__(text='Hello hello!')
+
+    transport.write(response.render())
+
+
+app = Application()
+
+r = app.get_router()
+r.add_route('/', slash)
+r.add_route('/hello', hello)
+
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser('server')
+    argparser.add_argument(
+        '-p', dest='flavor', default='block')
+    args = argparser.parse_args(sys.argv[1:])
+
+    app.serve(protocols.handler.make_class(args.flavor))
